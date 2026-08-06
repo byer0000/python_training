0a993465f1f7069a179a78a09ec0a8db38722a2e
Yuta <>
2026-08-06 16:51:43 +0900

    q1

--- engineering-practice/python/level1/q01_fizzbuzz/starter.py
+++ engineering-practice/python/level1/q01_fizzbuzz/starter.py
@@ -1,14 +1,27 @@
 def fizzbuzz_loop(n: int = 30) -> list:
     """for文とif文でFizzBuzzのリストを作る"""
-    # TODO: 実装する
-    pass
+    result = []
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            result.append('FizzBuzz')
+        elif i % 3 == 0:
+            result.append('Fizz')
+        elif i % 5 == 0:
+            result.append('Buzz')
+        else:
+            result.append(i)
 
+    return result
 
 def fizzbuzz_comprehension(n: int = 30) -> list:
     """リスト内包表記でFizzBuzzのリストを作る"""
-    # TODO: 実装する
-    pass
-
+    result = [
+        "FizzBuzz" if i % 3 == 0 and i % 5 == 0
+        else "Fizz" if i % 3 == 0
+        else "Buzz" if i % 5 == 0
+        else i for i in range(1, n + 1)
+    ]
+    return result
 
 if __name__ == "__main__":
     result_loop = fizzbuzz_loop()
