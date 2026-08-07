8def8c2172f25ec9c0cf84f170360799c88a3e57
Yuta <>
2026-08-07 09:34:31 +0900

    q7

--- engineering-practice/python/level2/q07_savings_account/savings_account.py
+++ engineering-practice/python/level2/q07_savings_account/savings_account.py
@@ -4,12 +4,13 @@ from bank_account import BankAccount
 class SavingsAccount(BankAccount):
     def __init__(self, owner: str, balance: float = 0, interest_rate: float = 0.0):
         # TODO: super().__init__() を呼び出し、interest_rate を保持する
-        pass
+        super().__init__(owner, balance)
+        self.interest_rate = interest_rate
 
     def add_interest(self) -> None:
         # TODO: balance に interest_rate分の利息を加算する
-        pass
-
+        interset = self.balance * self.interest_rate
+        self.deposit(interset)
 
 if __name__ == "__main__":
     acc = SavingsAccount("Yuta", 1000, interest_rate=0.05)
